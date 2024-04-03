7534be9876feaf6d03a43ae7829b22373f9eaaaf
Katlego Maphango <mphangokatlego08@gmail.com>
2024-04-03 22:51:15 +0200

    signup setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+import SignUpPage from "./pages/SignUpPage"
 
 function App() {
 
   return (
     <>
       <h1>App</h1>
+      <Routes>
+        <Route path="/signup" element={<SignUpPage />} />
+        
+      </Routes>
     </>
   )
 }
