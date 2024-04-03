681bb852dca658436d78e77462abf67e90dae883
Katlego Maphango <mphangokatlego08@gmail.com>
2024-04-03 23:19:00 +0200

    signup left

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import logo from '../assets/images/signup-img.jpg'
 
 const SignUpPage = () => {
-  return (
-    <div>SignUpPage</div>
-  )
+    return (
+        <>
+            <div className="signup">
+                <div className="signup-container">
+                    <div className="signup-left">
+                        <div className="signup-left_container">
+                            <img className="signup-left_img" src={logo} alt="signup image" />
+                        </div>
+                    </div>
+                    <div className="signup-right">
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default SignUpPage
